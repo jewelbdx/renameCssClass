47b33ce94e1db7e6120b2cc43e6799ef324739d1
Sam Lau <sam.suifai.lau@gmail.com>
2015-07-16 00:00:12 -0700

    handle command line usage

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 var chalk = require('chalk');
 var meow = require('meow');
 var pkg = require('./package.json');
+var path = require('path');
+var cssRenamer = require('./src/cssRenamer.js');
 
 var cli = meow({
     help: false,
@@ -16,13 +18,22 @@ var args = cli.input;
 var cmd = args[0];
 
 function init() {
-    if (Object.keys(flags).length === 0 && args.length === 0 && cmd === undefined) {
+    if ((Object.keys(flags).length === 0 
+            && args.length === 0 
+            && cmd === undefined) || args.length < 3) {
         defaultMessage();
     }
+
+    var str = args[0];
+    var replacement = args[1];
+    var dir = path.resolve(process.cwd(), args[2]);
+
+    cssRenamer.replaceAll(dir, str, replacement);
 }
 
 function defaultMessage() {
-    console.log('rename-css-class')
+    console.log('usage: '
+        + 'renameCssClass [pattern] [replacement] [file ...]')
 }
 
 // Execution
